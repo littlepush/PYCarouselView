b8973e654abc4226ac129d7e11d00b05a67fac6e
Push Chen <littlepush+github@gmail.com>
2017-05-19 15:18:07 +0800

    init project

---
+++ PYCarouselView/PYCarouselView.h
@@ -0,0 +1,19 @@
+//
+//  PYCarouselView.h
+//  PYCarouselView
+//
+//  Created by Push Chen on 19/05/2017.
+//  Copyright © 2017 PushLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PYCarouselView.
+FOUNDATION_EXPORT double PYCarouselViewVersionNumber;
+
+//! Project version string for PYCarouselView.
+FOUNDATION_EXPORT const unsigned char PYCarouselViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PYCarouselView/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PYCarouselView' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for PYCarouselView
+  pod 'PYUIKit'
+
+  target 'PYCarouselViewTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
